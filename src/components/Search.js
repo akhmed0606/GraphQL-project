62f0b658d227c58dea7fa0a8d30511591ce7ebce
akhmed0606 <analgiev57gmail.com>
2022-02-09 14:00:11 -0500

    add queries to search & pass argument from api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,34 @@
+import { useState } from 'react'
+import { useLazyQuery, gql } from "@apollo/client";
+
+const QUIERY_SEARCH = gql `
+  query Country($code: ID!) {
+         country(code: $code) {
+           name
+           capital
+           emoji
+           code
+           currency
+         }
+  }
+
+`
 
 
 const Search = () => {
-  return <div>
-      Search
+  const [ name, setName] = useState('')
+  const [ searchCountry, { data, loading, error}] = useLazyQuery(QUIERY_SEARCH, {variables: {code: searchCountry}})
+  
+  function handleChange(e) {
+     setName(e.target.value)
+  }
+  return <div className='search'>
+    <div className='inputs'>
+      <input type='text' placeholder='E.g RU' onChange={handleChange}/>
+      <button onClick={searchCountry}>Search Country</button>
+     
+    </div>
+   
   </div>;
 };
 
