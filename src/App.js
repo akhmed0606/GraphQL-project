d274a3f28e55bfdcbe2b4657437a19c55c696235
akhmed0606 <analgiev57gmail.com>
2022-02-08 20:35:59 -0500

    create new object with apollo and pass api url

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Search from './components/Search';
+import { ApolloClient,InMemoryCache,ApolloProvider} from '@apollo/client'
 
 function App() {
+  const client = new ApolloClient({
+      cache: new InMemoryCache(),
+      url: 'https://countries.trevorblades.com'
+  })
   return (
+    <ApolloProvider client={client}>
     <div className='App'>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/search' element={<Search />}></Route>
       </Routes>
     </div>
+    </ApolloProvider>
   );
 }
 
