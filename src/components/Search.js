dda5a40b3a4eb458e15d2896d96f9cb52a0cffa1
akhmed0606 <analgiev57gmail.com>
2022-02-09 14:27:57 -0500

    Check if data displaying correct

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,35 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { useLazyQuery, gql } from "@apollo/client";
 
-const QUIERY_SEARCH = gql `
+const QUIERY_SEARCH = gql`
   query Country($code: ID!) {
-         country(code: $code) {
-           name
-           capital
-           emoji
-           code
-           currency
-         }
+    country(code: $code) {
+      name
+      capital
+      emoji
+      code
+      currency
+    }
   }
-
-`
-
+`;
 
 const Search = () => {
-  const [ name, setName] = useState('')
-  const [ searchCountry, { data, loading, error}] = useLazyQuery(QUIERY_SEARCH, {variables: {code: searchCountry}})
-  
+  const [name, setName] = useState("");
+  const [searchCountry, { data, loading, error }] = useLazyQuery(
+    QUIERY_SEARCH,
+    { 
+      variables: { code: name.toUpperCase() } 
+    }
+  );
+
   function handleChange(e) {
-     setName(e.target.value)
+    setName(e.target.value);
   }
-  return <div className='search'>
-    <div className='inputs'>
-      <input type='text' placeholder='E.g RU' onChange={handleChange}/>
-      <button onClick={searchCountry}>Search Country</button>
-     
+  return (
+    <div className="search">
+      <div className="inputs">
+        <input type="text" placeholder="E.g RU" onChange={handleChange} />
+        <button onClick={searchCountry}>Search Country</button>
+      </div>
+      <div className="searchCountry">
+        {data && (
+          <div className="countryDisplay">
+            <h1>{data.country.name} {data.country.emoji}</h1>
+            <h1>Capital: {data.country.capital}</h1>
+            <h1>Currency: {data.country.currency}</h1>
+            <h1>Code: {data.country.code}</h1>
+          </div>
+        )}
+      </div>
     </div>
-   
-  </div>;
+  );
 };
 
 export default Search;
